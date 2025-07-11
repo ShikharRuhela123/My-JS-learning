33c19568e0646555edc755b6213457c40acb40cd
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-11 16:57:20 +0000

    practice of js

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,2 +1,21 @@
 console.log("shikhar");
 console.log("my name is gaurav");
+
+const name="shikhar";
+const repoCount=50;
+ console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('shikhar hc');
+console.log(gameName[0]);
+
+const url ="https\\shikhar.com\ shikhar%20ruhela";
+console.log(url.replace('%20','-'));
+console.log(url.includes('Shikhar'));  
+
+const user = {
+    name: "shikhar",
+    age : 20,
+    location : "aligarh",
+    email : 'shikhar'
+}
+console.log(user.location);
