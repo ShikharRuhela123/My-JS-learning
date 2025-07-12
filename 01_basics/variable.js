216196ebbec74a008cc452bd3e237a7bdc24cd1a
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-12 16:50:43 +0000

    practice on variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 144533;
+let accountEmail = "shikhar@google.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
