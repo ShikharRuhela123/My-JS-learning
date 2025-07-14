ece8f1983cba427706ae4c68752327cf90fddd33
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-14 11:41:43 +0000

    conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,18 @@
+let score = "33abc";
+let score1 = "33";
+let score2 = true;
+let score3 = false;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score); // for conversion of string value in number.
+let valueInNumberTwo = Number(score1);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//console.log(valueInNumberTwo);
+//console.log(score2);
+//console.log(score3);
+
+let isLoggedIn = "Shikhar";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
