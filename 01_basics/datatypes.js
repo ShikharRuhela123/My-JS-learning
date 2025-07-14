cd1c9a506bac181f38e941159282b110dcb2d2f2
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-14 11:11:57 +0000

    datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as a new version
+// alert(3+3) we are using nodejs, not browser.
+
+let name = "shikhar";
+let age = 18;
+let isLoggedIn = false;
+
+// some primitive datatype in js
+// number -> 2 to power 53
+// bigint 
+//string -> ""
+//boolean -> true\false
+//null -> standalone value
+//symbol -> unique purpose use
+// undefined
+
+//object
+
+console.log(typeof "shikhar");
+// for interview check type  ->  console.log(typeof null);
+// for interview check type  ->  console.log(typeof undefined);
