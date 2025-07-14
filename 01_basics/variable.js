6a1491cf5e7baefc54bebab2bf8f7afc1a6d2c76
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-14 06:54:20 +0000

    more practice on variable

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,9 +1,18 @@
 const accountId = 144533;
-let accountEmail = "shikhar@google.com";
+let accountEmail = "shikhar@google.com first email";
 var accountPassword = "12345";
 accountCity = "jaipur";
 
-console.log(accountId);
-console.log(accountEmail);
-console.log(accountPassword);
-console.log(accountCity);
+// this statement overwrite the first declaration  -> accountEmail = "gaurav@google.com second email";
+
+// constant value can't change in program like this -> accountId = 975683;
+
+
+//console.log(accountId);
+//console.log(accountEmail);
+//console.log(accountEmail2);
+//console.log(accountPassword);
+//console.log(accountCity);
+
+// Second method of console printing data in tabular form.  |
+console.table([accountId,accountEmail,accountPassword,accountCity]);
