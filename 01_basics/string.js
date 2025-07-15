cb3314b9e85aca42aee790eec54840e0f53d672b
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-15 12:05:59 +0000

    strings

---
+++ 01_basics/string.js
@@ -0,0 +1,22 @@
+const name = "shikhar";
+const repoCount = 50;
+ //console.log(name + repoCount + "value");  // concatting of string in this way is out of date
+
+ //console.log(`my name is ${name} and my repocount is ${repoCount}.`);
+
+const gameName = new String("shikhar123");
+//console.log(gameName);
+//console.log(gameName[2]);
+//console.log(gameName. __proto__);
+
+// Some String Property .................
+ //console.log(gameName.charAt(2));
+//console.log(gameName.toUpperCase());
+//console.log(gameName.length);
+
+//console.log(gameName.indexOf('k'));
+
+
+
+
+
