48672ff689c65c27c965e8d99661535a69b73462
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-15 11:40:55 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,23 @@
+ //console.log(2>1);
+ //console.log(2>=1);
+ //console.log(2!=1);
+  
+//.log("2" > 1); // javascript automatically convert 2 as a number
+//console.log("02" > 1);
+
+ //console.log(null > 0);
+ //console.log(null == 0);
+ //console.log(null >= 0);
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+
+ // === check conversion strictly with its datatypes...
+ console.log("2" === 2);
+ 
+
+ 
+ 
+ 
+ 
+
+
