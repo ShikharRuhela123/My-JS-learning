3afd3c823f0332deb0155d2db6791371e409c652
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-15 08:29:15 +0000

    operations

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -15,4 +15,30 @@ let valueInNumberTwo = Number(score1);
 
 let isLoggedIn = "Shikhar";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// ******************************* operations ************************************** 
+
+let value = 3;
+let negValue = -value;
+//onsole.log(negValue);
+// for calculate power use this code -> console.log(2**3);
+
+// string addition
+let str1 = "Shikhar";
+let str2 = " Ruhela";
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(3 + 4 * 5 % 3);  // prefer not use operatot like this 
+console.log((3 + 4) * 5 % 3);  // use like this
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3 = 2+2;
+console.log(num2);
+
