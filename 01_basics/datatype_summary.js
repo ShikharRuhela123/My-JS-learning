5246604dee985113cd511c4e5e05dfde4e8b0563
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-16 12:09:50 +0000

    datatypesummary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,21 @@
+
+// primitive datatype.....
+ const score = 100;
+ const scoreValue = 100.3;
+ const isLoggedIn = false;
+ const outsideTemp = null;
+ let userEmail;
+  
+ // symbol basic idea
+ const id =Symbol('123');      // demo of symbol.
+ const anotherId = Symbol('123');
+ console.log(id === anotherId);
+
+ const bigNumber = 454657889321456496123456n;  // at last n is used for converting in bigInt.
+ console.log(typeof bigNumber);
+
+ // Non primitive datatype or reference type
+
+ // array, object, function
+ 
+ 
