0f5440e06ca3415319842edba3cdadfaa2d4d0ef
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-17 16:48:10 +0000

    Dates

---
+++ 01_basics/Dates.js
@@ -0,0 +1,20 @@
+let myDate = new Date();
+
+  //console.log(myDate);
+  //console.log(typeof myDate);
+  //console.log(myDate.toLocaleString());
+  //.log(myDate.toString());
+
+ let myCreateDate = new Date(2025,2,23);
+ //console.log(myCreateDate);
+  //onsole.log(myCreateDate.toDateString());
+
+  let myCreatedDate =new Date(2025,2,17,5,3);   // Time bhi set kar skte hain 5 or 3 time ke liye hi hain
+  console.log(myCreatedDate.toLocaleString());
+  console.log(myCreatedDate.toDateString());
+  
+
+ 
+
+
+
