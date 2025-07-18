e5c055aa741e12cf37494437815944a489aecb92
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-18 11:54:44 +0000

    date more concept

--- 01_basics/Dates.js
+++ 01_basics/Dates.js
@@ -9,9 +9,28 @@ let myDate = new Date();
  //console.log(myCreateDate);
   //onsole.log(myCreateDate.toDateString());
 
-  let myCreatedDate =new Date(2025,2,17,5,3);   // Time bhi set kar skte hain 5 or 3 time ke liye hi hain
-  console.log(myCreatedDate.toLocaleString());
-  console.log(myCreatedDate.toDateString());
+  // let myCreatedDate =new Date(2025,2,17,5,3);   // Time bhi set kar skte hain 5 or 3 time ke liye hi hain
+  // console.log(myCreatedDate.toLocaleString());
+ // console.log(myCreatedDate.toDateString());
+
+  let myCreatedDate = new Date("01-14-2025")
+  // console.log(myCreatedDate.toLocaleString());
+
+  // +++++++++++++++++++TIMESTAMP IN DATE IN JAVASCRIPT+++++++++++++++++++++++++++++++
+
+  let myTimeStamp = Date.now();
+ // console.log(myTimeStamp);
+ // console.log(myCreatedDate.getTime());
+ // console.log(Math.floor(Date.now()/1000));
+
+ let newDate = new Date();
+ console.log(newDate);
+ console.log(newDate.getMonth());
+ console.log(newDate.getDay().toString());
+ 
+ 
+   
+  
   
 
  
