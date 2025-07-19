d74dad4455a1cb4ccd7d47a02321953419985f6a
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-19 16:53:46 +0000

    array

---
+++ 01_basics/array.js
@@ -0,0 +1,39 @@
+const myArr = [0,1,2,3,4,5];
+const myHeroes = ["shaktiman","naagraj"];
+ const myArr1 = new Array(1,2,3,4,5);
+ //console.log(myArr1[0]);
+
+ // Array Methods
+  myArr.push(6);
+  //console.log(myArr);
+
+   myArr.pop();
+  //console.log(myArr);
+   //myArr.pop();
+  //console.log(myArr);
+
+  myArr.unshift(9);
+  console.log(myArr);
+  //myArr.unshift(10);
+  //console.log(myArr);
+   
+  myArr.shift();
+  //console.log(myArr);
+ 
+  //console.log(myArr.includes(10));
+  //console.log(myArr.indexOf(9));
+  // console.log(myArr.indexOf(2));
+  
+   const newArr = myArr.join();
+   console.log(myArr);
+   console.log(newArr);
+   console.log(typeof newArr);
+   console.log(typeof myArr);
+   
+   
+   
+   
+  
+  
+  
+  
