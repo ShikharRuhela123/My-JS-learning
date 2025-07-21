889ca6664322e611d20de90cbf05be5c7ad7050c
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-21 07:52:21 +0000

    numbers

---
+++ 01_basics/numbers_and_math.js
@@ -0,0 +1,26 @@
+ const score = 400;
+// console.log(score);
+
+ const balance = new Number(100);
+ // console.log(balance);
+// console.log(balance.toString());
+  //console.log(balance.toString().length);
+ // console.log(balance.toFixed(4));
+
+  const otherNumber = 23.8966;
+  // console.log(otherNumber.toPrecision());
+  // console.log(otherNumber.toPrecision(3));
+  // console.log(otherNumber.toPrecision(4));
+  // console.log(otherNumber.toPrecision(2));
+  // console.log(otherNumber.toPrecision(1));
+  
+  const otherNumber1 = 1123.8966;
+  // console.log(otherNumber1.toPrecision(4));
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
