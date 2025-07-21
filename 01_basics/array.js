7e27182a08fd7bf84c3120edfd8ee1b238c02449
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-21 07:10:22 +0000

    more practice on arrays

--- 01_basics/array.js
+++ 01_basics/array.js
@@ -47,9 +47,24 @@ const myHeroes = ["shaktiman","naagraj"];
   const real_another_array = another_array.flat(Infinity);
   // console.log(real_another_array);
 
-  console.log(Array.isArray("hitesh"));
-  console.log(Array.from("hitesh"));
-  console.log(Array);
+  // console.log(Array.isArray("hitesh"));
+  // console.log(Array.from("hitesh"));
+  // console.log(Array);
+
+  console.log(Array.from({name : "hitesh"}));
+
+  let score1 = 100;
+  let score2 = 200;
+  let score3 = 300;
+  const arr = Array.of(score1,score2,score3);
+  console.log(arr);
+  console.log(typeof arr);
+  
+  
+  
+  
+
+
   
   
   
