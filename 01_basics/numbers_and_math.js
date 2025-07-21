c7ac89d066ba72681eb33ad9d27b876be4c78927
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-21 09:31:36 +0000

    math

--- 01_basics/numbers_and_math.js
+++ 01_basics/numbers_and_math.js
@@ -16,6 +16,28 @@
   
   const otherNumber1 = 1123.8966;
   // console.log(otherNumber1.toPrecision(4));
+
+  const hundreds = 100000000;
+  // console.log(hundreds.toLocaleString());
+  // console.log(hundreds.toLocaleString('en-IN'));
+
+  const myNum = [0,1,4,8,9,10];
+  // console.log(myNum.MAX_VALUE());
+
+  // +++++++++++++++++++++++++ Math in javascript +++++++++++++++++++++++++++++
+
+  console.log(Math);
+  console.log(Math.abs(-4));
+  console.log(Math.round(4.6));
+  console.log(Math.ceil(4.2));
+  console.log((Math.floor(4.9)));
+  console.log(Math.min(4,3,5,6,9));
+  
+  
+  
+  
+  
+  
   
   
   
