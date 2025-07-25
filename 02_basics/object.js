43b3500b1acc8071fefabb9ac43da5d164e9c2ae
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-25 08:16:45 +0000

    objects

---
+++ 02_basics/object.js
@@ -0,0 +1,27 @@
+ const jsuser = {
+    name : "shikhar",
+    "full name" : "shikhar ruhela",  // how to access this in dot method thats why we use second method
+    age : 18,
+    location : "Aligarh",
+    email : "shikhar@google.com",
+    isLoogedIn : false,
+    lastLoginDays : ["monday" , "saturady"]
+ }
+  
+//  console.log(jsuser.age);
+ 
+//  console.log(jsuser["isLoogedIn"]); // second method for access object element..
+//  console.log(jsuser["full name"]);
+ 
+ const jsuser1 = {
+     name : "shikhar",
+    "full name" : "shikhar ruhela",
+     age : 18,
+    mySym : "myKey 1",
+    location : "Aligarh"
+ }
+ console.log(jsuser1["mySym"]);
+  console.log(typeof jsuser1["mySym"]);
+ 
+
+ 
