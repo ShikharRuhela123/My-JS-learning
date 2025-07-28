7de5cd78d1a8c93aa2ebfdb8a9387afbbc8e4b70
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-28 12:43:56 +0000

    functions

---
+++ 02_basics/function.js
@@ -0,0 +1,24 @@
+function welcome() {
+    let greeting = "Welcome sir for coming in this scope . i hope u enjoy our services."
+    let name = "shikhar";
+    let wel = `Hello ${greeting}, Thank you ${name}`;
+    console.log(wel);
+    
+}
+
+// console.log(welcome());
+
+function addTwoNumbers(number1,number2) {
+   let result = number1 + number2;
+    return result
+}
+
+// console.log(addTwoNumbers(3,7));
+// console.log(addTwoNumbers(3,"s"));
+// console.log(addTwoNumbers(3,null));
+
+const result1 = addTwoNumbers(3,7);
+console.log("RESULT:",result1);
+
+
+
