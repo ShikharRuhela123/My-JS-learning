03770dc5b098439b71415ddb89de2bdc230e48fc
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-29 12:29:27 +0000

    work on function

--- 02_basics/function.js
+++ 02_basics/function.js
@@ -22,23 +22,43 @@ const result1 = addTwoNumbers(3,7);
 // console.log("RESULT:",result1);
 
 function loginUserMessage(username){
-    if(!username === undefined){
+    if(username === undefined){
         console.log("please enter a username");
-        return
+        return;
     }
-    return `${username} just logged in.`
+    return `${username} just logged in.`;
 }
 
 // loginUserMessage("shikhar");
 // console.log(loginUserMessage("Shikhar"));
-// console.log(loginUserMessage(" "));
+//  console.log(loginUserMessage(" "));
 // console.log(loginUserMessage());
 
-console.log(loginUserMessage("Shikhar"));
+ function loginUserMessage(username = "virat"){
+    if(!username === undefined){
+        console.log("please enter a username");
+        return;
+    }
+    return `${username} just logged in.`;
+}
+// console.log(loginUserMessage());
 
+function calculateCartPrice(num1){
+    return num1;
+}
+// console.log(calculateCartPrice(5));  // here are only single value in cart , but problem create if user enter multiple value
+// console.log(calculateCartPrice(200, 500, 900, 999)); // here create problem only single value is print .
+// solve this problem use rest operator(...)
 
+function calculateCartPrice(...num1){
+    return num1;
+}
+// console.log(calculateCartPrice(200, 500, 900, 999)); 
 
-
+function calculate(val1, val2, val3, ...num1){
+    return num1;
+}
+console.log(calculate(200,400,5000,1000));
 
 
 
