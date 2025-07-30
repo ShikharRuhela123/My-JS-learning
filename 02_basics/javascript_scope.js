b1c7066a040f42cb6c455dbaed83c41a849c77bd
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-30 08:41:59 +0000

    work on scope

--- 02_basics/javascript_scope.js
+++ 02_basics/javascript_scope.js
@@ -7,14 +7,25 @@
 // console.log(c);
 
 // {} this is scope
-
+// var c= 300;
+let a = 300;
 if(true){
 let a = 10;
 const b= 20;
-var c = 30;
+// console.log("inner value: ",a);
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);  // here create a problem yeh c variable ish scope se bhar aana hi nhi chaiye tha.
 
+function one(){
+    const username = "shikhar";
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
