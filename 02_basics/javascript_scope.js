acd7264dfd1042ed45ea55143da683206f19ac48
ShikharRuhela123 <shikharruhela360@gmail.com>
2025-07-30 11:33:08 +0000

    more understanding of scope

--- 02_basics/javascript_scope.js
+++ 02_basics/javascript_scope.js
@@ -23,9 +23,35 @@ function one(){
     const username = "shikhar";
     function two(){
         const website = "youtube";
-        console.log(username);
+        // console.log(username);
     }
     // console.log(website);
     two()
 }
 one()
+
+
+if(true){
+    const username = "shikhar";
+    if(username === "shikhar"){
+        const website = " youtube";
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(5));
+function addOne(num){
+    return num + 1;
+}
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2;
+}
+
